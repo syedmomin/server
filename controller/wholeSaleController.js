47631e9833b76b998451540bd22cc6ddbd548c5e
momin_finspace <s.momin@finspacegroup.co.uk>
2023-09-03 14:10:36 +0500

    wholesale route compelte

---
+++ controller/wholeSaleController.js
@@ -0,0 +1,307 @@
+const db = require("../config/dbConnection");
+
+const collection = {
+  create: async function (req, res) {
+    try {
+      const {
+        transactionDate,
+        receivingDate,
+        supplierName,
+        supplierNumber,
+        totalShipmentCost,
+        totalMiscCost,
+        totalCost,
+        totalNetAmount,
+        note,
+        details,
+      } = req.body;
+
+      await db.query(
+        "INSERT INTO wholesale_master (transactionDate,receivingDate,supplierName,supplierNumber,totalShipmentCost,totalMiscCost,totalCost,totalNetAmount,note) VALUES (?,?,?,?,?,?,?,?,?)",
+        [
+          transactionDate,
+          receivingDate,
+          supplierName,
+          supplierNumber,
+          totalShipmentCost,
+          totalMiscCost,
+          totalCost,
+          totalNetAmount,
+          note,
+        ],
+        async (error, results) => {
+          if (error) {
+            res.status(500).send({
+              code: 500,
+              status: false,
+              message: error,
+            });
+          }
+          const masterId = results.insertId;
+          try {
+            await Promise.all(
+              details.map(async (detail) => {
+                await db.query(
+                  "INSERT INTO wholesale_detail (masterId,itemMaster,itemUOM,itemArticle,itemColor,itemQuantity,itemRate,itemAmount,itemShipment,itemMiscCost,itemNetRate,itemNetAmount,itemImage) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                  [
+                    masterId,
+                    detail.itemMaster,
+                    detail.itemUOM,
+                    detail.itemArticle,
+                    detail.itemColor,
+                    detail.itemQuantity,
+                    detail.itemRate,
+                    detail.itemAmount,
+                    detail.itemShipment,
+                    detail.itemMiscCost,
+                    detail.itemNetRate,
+                    detail.itemNetAmount,
+                    detail.itemImage,
+                  ]
+                );
+              })
+            );
+
+            return res.status(200).send({
+              code: 200,
+              status: true,
+              message: "Add GRN Successfully",
+            });
+          } catch (error) {
+            return res.status(500).send({
+              code: 500,
+              status: false,
+              message: error,
+            });
+          }
+        }
+      );
+    } catch (error) {
+      res.status(500).send({
+        status: false,
+        code: 500,
+        message: error.message,
+      });
+    }
+  },
+  update: async function (req, res) {
+    try {
+      const id = req.body.id;
+      const detail = req.body.details;
+      const updateColumns = req.body;
+      delete updateColumns.id;
+      delete updateColumns.details;
+      const updateSql = `UPDATE wholesale_master SET ${Object.keys(
+        updateColumns
+      )
+        .map((key) => `${key} = ?`)
+        .join(", ")} WHERE id = ?`;
+      const updateValues = [...Object.values(updateColumns), id];
+
+      await db.query(updateSql, updateValues, async (error, results) => {
+        if (error) {
+          res.status(500).send({
+            code: 500,
+            status: false,
+            message: error,
+          });
+        }
+        await Promise.all(
+          detail.map(async (update) => {
+            await db.query(
+              `UPDATE wholesale_detail SET itemMaster = ?, itemUOM = ?, itemArticle = ?, itemColor = ?,itemQuantity = ?,itemRate = ?,itemAmount = ?,itemShipment = ?,itemMiscCost = ?,itemNetRate = ?,itemNetAmount = ?,itemImage = ? WHERE id = ?`,
+              [
+                update.itemMaster,
+                update.itemUOM,
+                update.itemArticle,
+                update.itemColor,
+                update.itemQuantity,
+                update.itemRate,
+                update.itemAmount,
+                update.itemShipment,
+                update.itemMiscCost,
+                update.itemNetRate,
+                update.itemNetAmount,
+                update.itemImage,
+                update.id,
+              ]
+            );
+          })
+        );
+        if (results.affectedRows > 0) {
+          res.status(200).send({
+            code: 200,
+            status: true,
+            message: "Update record successfully",
+          });
+        } else {
+          res.status(206).send({
+            code: 206,
+            status: false,
+            message: "This Id Not Exist!",
+          });
+        }
+        // else {
+        //   if (results.affectedRows > 0) {
+        //     res.status(200).send({
+        //       code: 200,
+        //       status: true,
+        //       message: "Update record successfully",
+        //     });
+        //   } else {
+        //     res.status(206).send({
+        //       code: 206,
+        //       status: false,
+        //       message: "This Id Not Exist!",
+        //     });
+        //   }
+        // }
+      });
+    } catch (error) {
+      res.status(500).send({
+        status: false,
+        code: 500,
+        message: error.message,
+      });
+    }
+  },
+  delete: async function (req, res) {
+    try {
+      await db.query(
+        "DELETE FROM wholesale_master WHERE id = ?",
+        [req.body.id],
+        async (error, results) => {
+          if (error) {
+            res.status(500).send({
+              code: 500,
+              status: false,
+              message: error,
+            });
+          }
+          await db.query(
+            "DELETE FROM wholesale_detail WHERE masterId = ?",
+            [req.body.id],
+            (error, allDelete) => {
+              if (error) {
+                res.status(500).send({
+                  code: 500,
+                  status: false,
+                  message: error,
+                });
+              }
+            }
+          );
+
+          if (results.affectedRows > 0) {
+            res.status(200).send({
+              code: 200,
+              status: true,
+              message: "Delete record successfully",
+            });
+          } else {
+            res.status(206).send({
+              code: 206,
+              status: false,
+              message: "This Id Not Exist!",
+            });
+          }
+        }
+      );
+    } catch (error) {
+      res.status(500).send({
+        status: false,
+        code: 500,
+        message: error.message,
+      });
+    }
+  },
+  getAll: async function (req, res) {
+    try {
+      await db.query(
+        "SELECT * FROM wholesale_master",
+        async (error, results) => {
+          if (error) {
+            res.status(500).send({
+              code: 500,
+              status: false,
+              message: error,
+            });
+          } else {
+            if (results.length > 0) {
+              res.status(200).send({
+                code: 200,
+                status: true,
+                message: "Get all records",
+                data: results,
+              });
+            } else {
+              res.status(206).send({
+                code: 206,
+                status: false,
+                message: "Record Not Exist!",
+              });
+            }
+          }
+        }
+      );
+    } catch (error) {
+      res.status(500).send({
+        status: false,
+        code: 500,
+        message: error.message,
+      });
+    }
+  },
+  getById: async function (req, res) {
+    try {
+      await db.query(
+        "SELECT * FROM wholesale_master WHERE id = ?",
+        [req.body.id],
+        async (error, results) => {
+          if (error) {
+            res.status(500).send({
+              code: 500,
+              status: false,
+              message: error,
+            });
+          }
+          await db.query(
+            "SELECT * FROM wholesale_detail WHERE masterId = ?",
+            [req.body.id],
+            (error, detailOrder) => {
+              if (error) {
+                res.status(500).send({
+                  code: 500,
+                  status: false,
+                  message: error,
+                });
+              }
+              if (results.length > 0) {
+                res.status(200).send({
+                  code: 200,
+                  status: true,
+                  message: "Get details by id",
+                  data: { ...results[0], details: detailOrder },
+                });
+              } else {
+                res.status(206).send({
+                  code: 206,
+                  status: false,
+                  message: "This Id Not Exist!",
+                });
+              }
+            }
+          );
+        }
+      );
+    } catch (error) {
+      res.status(500).send({
+        status: false,
+        code: 500,
+        message: error.message,
+      });
+    }
+  },
+};
+
+module.exports = collection;
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -14,6 +14,7 @@ router.use("/color-type", require("./colorTypeRoute"));
 router.use("/placket-type", require("./placketTypeRoute"));
 router.use("/item-master", require("./itemMasterRoute"));
 router.use("/grn", require("./grnRoute"));
+router.use("/wholeSale", require("./wholeSaleRoute"));
 router.use("/order", require("./orderRoute"));
 router.use("/karigar-salary", require("./karigarSalaryRoute"));
 router.use("/expenses-ledger", require("./expensesLedgerRoute"));
